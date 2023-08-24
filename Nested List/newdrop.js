1dbbd7d387ab5dabd85c92f66fd40d0e415a13a7
yash chouhan <yashchouhan@192.168.1.12>
2023-08-25 00:28:27 +0530

    check

--- Nested List/newdrop.js
+++ Nested List/newdrop.js
@@ -3,6 +3,7 @@ const section = document.querySelector(".section");
 const form = document.querySelector(".form-category");
 const input_text = document.querySelector("#input");
 const mainul = document.querySelector(".mainul");
+loadFromLocalStorage()
 form.addEventListener("submit", (e) => {
     e.preventDefault();   
     const inputValue = input_text.value;
@@ -14,11 +15,9 @@ form.addEventListener("submit", (e) => {
     const innerLiHTML = `    <li>
     
                <h5 class="" >${inputValue}</h5>
-                <input id="text-category"  class="text-category"/>
                 <div class="btn"> 
                 <button type="button" class="btn btn-success btn-sm" onclick="libutton()">AddChild</button>
                 <button type="button" class="btn btn-success btn-sm" onclick="addSibling()">AddSibling</button>
-                <button type="button" class="btn btn-primary btn-sm" onclick="edit()">Edit</button>
                 <button type="button" class="btn btn-danger btn-sm" onclick="del()">Delete</button>
                 </div>
  
@@ -26,27 +25,13 @@ form.addEventListener("submit", (e) => {
 
     newul.innerHTML = innerLiHTML;
     mainul.append(newul);
-
-    //localstorage
-    let task=localStorage.getItem("parent")
-    let myobj=[]
-    myobj=JSON.parse(task)
-    myobj.push({"parent":inputValue})
-    // Object.assign(myobj,{"parent":inputValue})
-    console.log(myobj)
-    // maincat = JSON.parse(task);
-    // maincat.push(inputValue)
-    localStorage.setItem("parent",JSON.stringify(myobj))
+    saveToLocalStorage()
     input_text.value = "";
 
 });
 //add next child
 function libutton() {
-    
     let inputValue = event.target.parentNode.parentNode.children[1].value
-    // console.log(inputval)
-    // const inputValue = newinputval;
-    // console.log(inputValue)
     const newLi = document.createElement("li");
     newLi.setAttribute("ondragstart","dragStart(event)")
     newLi.setAttribute("draggable","true")
@@ -68,19 +53,12 @@ function libutton() {
     newLi.innerHTML = innerLiHTML;
     let li = event.target.parentNode.parentNode;
     li.appendChild(newLi);
+    saveToLocalStorage()
     event.target.parentNode.parentNode.children[1].value = "";
-
-   //localstorage
-    parenthead=event.target.parentNode.parentNode.children[0].innerHTML
-    let task=localStorage.getItem("parent")
-    myobj = JSON.parse(task);
-    myobj.push({[parenthead]:inputValue})
-    localStorage.setItem("parent",JSON.stringify(myobj))
 }
 //add sibling
 function addSibling(){
     const inputValue = event.target.parentNode.parentNode.children[1];
-    console.log(inputValue)
     const newul = document.createElement("ul");
     newul.setAttribute("id","ul")
     newul.setAttribute("ondragstart","dragStart(event)")
@@ -100,20 +78,14 @@ function addSibling(){
     newul.innerHTML = innerLiHTML;
     let sib = event.target.parentNode.parentNode.parentNode;
     sib.append(newul);
+    saveToLocalStorage()
     inputValue.value = "";
 }
 //delete button
 function del() {
     let ul = event.target.parentNode.parentNode.parentNode;
-    // console.log(ul)
     ul.remove();
-    let task=localStorage.getItem("parent")
-    myobj =[];
-    myobj = JSON.parse(task);
-    
-    localStorage.setItem("parent",JSON.stringify(myobj))
-
-// console.log(maincat)
+    saveToLocalStorage()
 }
 //drag and drop
 let div;
@@ -126,36 +98,21 @@ function dragOver(event) {
     over.append(div);
 }
 
-
-function edit(){
-    
-    let newEditNode = event.target.parentNode.parentNode.children;
-    if(newEditNode[1].value.trim()!=""){
-        let changeSpan= newEditNode[0];
-        changeSpan.innerHTML = newEditNode[1].value;
-        input_text.value = "";
-        newEditNode[1].value="";
-        
-    }
-
-  }
-
   function deleteAll(){
-    
-   
-    let task=localStorage.getItem("parent")
-    myobj = JSON.parse(task);
-myobj=[]
-    localStorage.setItem("parent",JSON.stringify(myobj))
     document.getElementsByClassName('mainul')
     mainul.innerHTML=""
+    saveToLocalStorage()
   }
 
+  function saveToLocalStorage() {
+    const mainulHTML = mainul.innerHTML;
+    localStorage.setItem('savedHTML', mainulHTML);
+}
 
-  function displayData(){
-
-
-
-
-  }
-
+// Loading HTML from Local Storage
+function loadFromLocalStorage() {
+    const savedHTML = localStorage.getItem('savedHTML');
+    if (savedHTML) {
+        mainul.innerHTML = savedHTML;
+    }
+}
